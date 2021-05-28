99b436d80ba40733d2a9adbc5799d1a0d59cc353
James Lasher <jim.lasher@q.com>
2021-05-28 16:54:10 -0600

    initial

---
+++ pi_progress.py
@@ -0,0 +1,67 @@
+import json
+import os
+
+from Lib.jira import jira
+
+# dataPath = os.path.join(os.getcwd(), 'data', 'sqlite.db')
+# print(dataPath)
+
+# Provide an absolute path.  Otherwise, ../details.json will be used.
+def getAuthInfo(filepath=None):
+    if filepath is None:
+        parent = os.path.abspath(os.path.join(os.getcwd(), '..'))
+        filepath = os.path.join(parent, 'details.json')
+
+    with open(filepath, 'r') as input:
+        data = json.load(input)
+        email = data['email']
+        token = data['token']
+        return(email, token)
+
+
+def getIssueById(id):
+    json = conn.getIssueById(id)
+    print(json['key'])
+    print(json['fields']['summary'])
+    print(json['fields']['issuetype']['name'])
+    print(json['fields']['status']['name'])
+    print(json['fields']['timeoriginalestimate'])
+
+    # sprints
+    sprints = json['fields']['customfield_10020']
+
+    if sprints is not None:
+        for sp in sprints:
+            print(sp['name'])
+            print(sp['id'])
+    
+    # epic
+    print('Epic: ' + json['fields']['customfield_10014'])
+
+    print('\nSubtasks')
+    subtask = json['fields']['subtasks']
+    for s in subtask:
+        print()
+        print(s['key'])
+        print(s['fields']['summary'])
+        print(s['fields']['issuetype']['name'])
+        print(s['fields']['status']['name'])
+
+(email, token) = getAuthInfo()
+conn = jira(email, token)
+#getIssueById('FCDRM-1514')
+
+#json = conn.agileRequest('board/5/backlog', { 'startAt': '350', 'maxResults': '50' })
+#print(len(json['issues']))
+
+epics = ['FCDRM-2046', 'FCDRM-2047', 'FCDRM-1266', 'FCDRM-1246', 'FCDRM-2114', 'FCDRM-2199', 'FCDRM-2137',
+        'FCDRM-1198', 'FCDRM-1349', 'FCDRM-1217', 'FCDRM-2271', 'FCDRM-2101', 'FCDRM-2119', 'FCDRM-2132', 
+        'FCDRM-2270', 'FCDRM-2145', 'FCDRM-2148', 'FCDRM-1175', 'FCDRM-2194', 'FCDRM-2273', 'FCDRM-2201',
+        'FCDRM-1397', 'FCDRM-1396', 'FCDRM-1392', 'FCDRM-1388', 'FCDRM-2283', 'FCDRM-2240', 'FCDRM-2109', 
+        'FCDRM-1269', 'FCDRM-2581']
+epics = ','.join(epics)
+query = f'issuetype=Epic+AND+key+in({ epics })'
+print(query)
+json = conn.getJqlResults(query)
+print(json)
+
---
+++ venv/Lib/jira.py
@@ -0,0 +1,54 @@
+import base64
+import json
+import requests
+
+class jira:
+    _base = 'https://brillient.atlassian.net/rest/'
+    _rest_base = _base + 'api/3/'
+    _agile_base = _base + 'agile/1.0/'
+
+    def __init__(self, username, token=None):
+        if token is None:
+            self.token = username
+        else:
+            self.token = self._generateAuthToken(username, token)
+
+    def _generateAuthToken(self, username, token):
+        input = bytearray((':'.join([username, token])).encode())
+        authToken = base64.b64encode(input).decode('utf-8')
+        return authToken
+
+
+    def restRequest(self, url, data=None):
+        return self.request(self._rest_base + url, data)
+
+    def agileRequest(self, url, data=None):
+        return self.request(self._agile_base + url, data)
+
+    # For now, the data value will be a dictionary for building the query string
+    def request(self, url, data=None):
+        headers = {'Authorization': 'Basic ' + self.token,
+            'Content-Type': 'application/json'}
+
+        # Build the query string, if needed
+        if data is not None:
+            qryString = []
+
+            # TODO: HTML escape values
+            for key, val in data.items():
+                qryString.append(f'{key}={val}')
+
+            url = url + '?' + '&'.join(qryString)
+
+        response = requests.get(url, headers=headers)
+
+        if response.status_code == 200:
+            return json.loads(response.content.decode('utf-8'))
+        else:
+            print('ERROR: ' + response.content.decode('utf-8'))
+
+    def getIssueById(self, id):
+        return self.restRequest(f'issue/{id}')
+
+    def getJqlResults(self, query):
+        return self.restRequest('search', { 'jql': query})
